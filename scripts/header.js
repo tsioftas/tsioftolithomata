6c75c2e33492db9a764c14567a91197277008050
tsioftas <panaevri@gmail.com>
2025-03-14 04:43:45 +0200

    added scripts

---
+++ scripts/header.js
@@ -0,0 +1,63 @@
+let pages = [];
+
+const headerLoadedEvt = new Event("headerLoaded")
+
+fetch("http://192.168.1.2:8000/jsondata/pages.json").then((resp) => resp.json()).then((json) => {
+    pages = json["pages"];
+}).catch((err) => console.error(err));
+
+// stolen from language.js
+function getLanguage() {
+  return localStorage.getItem('language') || 'el'; // Προεπιλεγμένη γλώσσα τα Ελληνικά
+}
+
+function pageToKey(page) {
+  return page.name.toLowerCase().replace(" ", "_")
+}
+
+fetch('http://192.168.1.2:8000/templates/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-container').innerHTML = data;
+    window.dispatchEvent(headerLoadedEvt)
+    // search
+    fetch('http://192.168.1.2:8000/jsondata/dict.json')
+      .then(response => response.json())
+      .then(translateDict => {
+        const searchInput = document.getElementById('search-input');
+        const searchResults = document.getElementById('search-results');
+        searchInput.addEventListener('input', () => {
+          const searchTerm = searchInput.value.toLowerCase();
+          searchResults.innerHTML = ''; 
+      
+          if (searchTerm.length >= 3) {
+            const results = pages.filter(page => {
+              const key = pageToKey(page);
+              return _LANGUAGES.map((lang) => {
+                const translation = translateDict[lang][key];
+                console.assert(translation, `Missing translation for '${key}' in language '${lang}'.`);
+                return translation.toLowerCase();
+              }).some((s) => s.includes(searchTerm))
+            });
+        
+            if (results.length > 0) {
+              searchResults.style.display = 'block';
+              results.forEach(result => {
+                const li = document.createElement('li');
+                const siteLanguage = getLanguage();
+                li.textContent = translateDict[siteLanguage][pageToKey(result)];
+                li.addEventListener('click', () => {
+                  window.location.href = result.path;
+                });
+                searchResults.appendChild(li);
+              });
+            } else {
+              searchResults.style.display = 'none';
+            }
+          } else {
+            searchResults.style.display = 'none';
+          }
+        });
+    });
+});
+
---
+++ scripts/language.js
@@ -0,0 +1,111 @@
+//language.js
+var doc = document;
+let navPathLoaded = false;
+let docpath = "";
+let globalDict = {};
+
+const _LANGUAGES = ["el", "en"];
+
+function getPathUnit(item_name, translated) {
+    if (item_name == "home") {
+        return capitalize(translated);
+    } else {
+      return "/ " + capitalize(translated);
+    }
+}
+
+// Function to set the language
+function setLanguage(lang) {
+    localStorage.setItem('language', lang);
+    applyLanguage(lang);
+  }
+  
+  // Function to get the language
+  function getLanguage() {
+    return localStorage.getItem('language') || 'el'; // Προεπιλεγμένη γλώσσα τα Ελληνικά
+  }
+  
+  // Function to apply the selected language to the page
+  function applyLanguage(lang) {
+    const thisScript = document.getElementById('language-script');
+    dictPath = thisScript.getAttribute('dict');
+    keys = thisScript.getAttribute('keys').split(',');
+    galleryLength = Number(thisScript.getAttribute('galleryLength'));
+    fetch("http://192.168.1.2:8000" + dictPath)
+    .then(response => response.json())
+    .then(translations => {
+        keys.forEach(key => {
+            const elem = doc.getElementById(key);
+            if (elem) {
+              elem.textContent = translations[lang][key];
+            } else {
+              console.error("Missing element \"" + key + "\" from page")
+            }
+        })
+        // Ειδική περίπτωση για την γκαλερί
+        if (galleryLength > 0) {
+            gallery = doc.getElementById('gallery').gal;
+            for (let i = 1; i <= galleryLength; i++) {
+                item = doc.getElementById('gallery-' + i);
+                const translatedCaption = translations[lang]['gallery'][i-1];
+                item.setAttribute('data-sub-html', translatedCaption);
+            }
+
+            // Destroy the existing lightGallery instance
+            gallery.destroy();
+
+            // Re-initialize lightGallery
+            initializeGallery();
+        }
+        // Ειδική περίπτωση για το μονοπάτι πλοήγησης
+        if (navPathLoaded) {
+          const pathElement = document.getElementById('navpath');
+          pathElement.innerHTML = "";
+          docpath.forEach((item, index) => {
+            const translated = globalDict[lang][item.name];
+            console.assert(translated, `Missing translation for ${item.name} in language '${lang}'.`)
+            pathElement.innerHTML = pathElement.innerHTML + `<a href="${item.link}"> ${getPathUnit(item.name, translated)}</a>`;
+          });
+        }
+        // Υποσέλιδο      
+        if ('footer-name' in globalDict[lang]) {
+          const footerName = document.getElementById('footer-name');  
+          footerName.innerText = globalDict[lang]['footer-name'];
+        } else {
+          console.error(`Missing translation for footer-name in language '${lang}'.`);
+        }
+    });
+}
+
+// On page load, apply the selected language
+window.addEventListener('DOMContentLoaded', () => {
+  const lang = getLanguage();
+  applyLanguage(lang);
+});
+
+window.addEventListener('headerLoaded', () => {
+  // Add event listeners to the language buttons
+  const enButton = document.getElementById('en-button');
+  enButton.innerHTML = "<div class=\"container\"><div><image src=\"/images/flags/UK.png\" width=20></image></div><div class=\"lang-button\">English </div></div>"
+  enButton.addEventListener('click', () => {
+    setLanguage('en');
+  });
+  
+  const elButton = document.getElementById('el-button')
+  elButton.innerHTML = "<div class=\"container\"><div><image src=\"/images/flags/GR.png\" width=20></image></div><div class=\"lang-button\">Ελληνικά </div></div>"
+  elButton.addEventListener('click', () => {
+    setLanguage('el');
+  });
+});
+
+fetch("http://192.168.1.2:8000/jsondata/dict.json")
+  .then((response) => response.json()
+  .then((jsondict) => {
+      globalDict = jsondict;
+      window.addEventListener('navPathLoaded', () => {
+      const pathElement = document.getElementById('navpath');
+      docpath = pathElement.path;
+      navPathLoaded = true;
+      applyLanguage(getLanguage());
+    });
+  }));
