793329f8522e9a2ed709d950bbda2e75fed579fc
tsioftas <panaevri@gmail.com>
2025-05-07 20:11:07 +0100

    various improvements (#27)
    - remove more image metadata
    - add more images of fish sample
    - rename dirs to keep id info of other collection
    - use greek numbering in ancient greek translation

--- scripts/random-sample.js
+++ scripts/random-sample.js
@@ -217,15 +217,18 @@ samples = [
         "link_to_species": "/tree/animalia/chordata/chondrichthyes/elasmobranchii/odontaspididae/sylvestrilamia/sylvestrilamia_teretidens/sylvestrilamia_teretidens.html",
     },
     {
-        "image_path": "/images/unclassified/sample1",
+        "image_path": "/images/unclassified/sample1(7)",
         "images": [
             "U1_1.jpg",
+            "U1_2.jpg",
+            "U1_3.jpg",
+            "U1_4.jpg",
         ],
         "species": "?actinopterygii",
         "link_to_species": "/tree/animalia/chordata/osteichthyes/actinopterygii/actinopterygii.html",
     },
     {
-        "image_path": "/images/unclassified/sample2",
+        "image_path": "/images/unclassified/sample2(3)",
         "images": [
             "U2_1.jpg",
         ],
